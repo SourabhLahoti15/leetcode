4dde79a25662a2f83ab2b05c9f13c18e7cc04587
Sourabh Lahoti <149955947+SourabhLahoti15@users.noreply.github.com>
2025-12-17 10:51:04 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 12/17/2025, 10:51:08 AM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return ("Hello World");
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
