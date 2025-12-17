89b0b6831801a86bc521c33faa4a8028026e2c08
Sourabh Lahoti <149955947+SourabhLahoti15@users.noreply.github.com>
2025-12-17 10:52:00 +0530

    Create 745-FindSmallestLetterGreaterThanTarget

---
+++ 745-FindSmallestLetterGreaterThanTarget/745-FindSmallestLetterGreaterThanTarget.py
@@ -0,0 +1,12 @@
+# Last updated: 12/17/2025, 10:52:05 AM
+class Solution(object):
+    def nextGreatestLetter(self, letters, target):
+        """
+        :type letters: List[str]
+        :type target: str
+        :rtype: str
+        """
+        for i in letters:
+            if ord(i) > ord(target):
+                return i
+        return letters[0]
