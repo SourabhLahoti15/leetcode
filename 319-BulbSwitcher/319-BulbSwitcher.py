6eab02617cf785bb9e220fc0f937ac32a2aff982
Sourabh Lahoti <149955947+SourabhLahoti15@users.noreply.github.com>
2025-12-17 10:52:50 +0530

    Create 319-BulbSwitcher

---
+++ 319-BulbSwitcher/319-BulbSwitcher.py
@@ -0,0 +1,8 @@
+# Last updated: 12/17/2025, 10:52:54 AM
+class Solution(object):
+    def bulbSwitch(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return int(n**0.5)
