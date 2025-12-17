8834c4d5ea642bf34b460f57291c0529d29479c8
Sourabh Lahoti <149955947+SourabhLahoti15@users.noreply.github.com>
2025-12-17 10:52:30 +0530

    Create 415-AddStrings

---
+++ 415-AddStrings/415-AddStrings.py
@@ -0,0 +1,10 @@
+# Last updated: 12/17/2025, 10:52:34 AM
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        return str(int(num1) + int(num2))
+        
