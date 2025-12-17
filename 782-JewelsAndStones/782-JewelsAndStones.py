6645e51b5c8e08254a31be5a4d69e07511c8b0d5
Sourabh Lahoti <149955947+SourabhLahoti15@users.noreply.github.com>
2025-12-17 10:51:59 +0530

    Create 782-JewelsAndStones

---
+++ 782-JewelsAndStones/782-JewelsAndStones.py
@@ -0,0 +1,14 @@
+# Last updated: 12/17/2025, 10:52:03 AM
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        count = 0
+        for c in stones:
+            if c in jewels:
+                count += 1
+        return count
+        
