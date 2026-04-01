de9275e3b4f56ec672e7395c8feb3cef68de6928
Sourabh Lahoti <149955947+SourabhLahoti15@users.noreply.github.com>
2026-04-01 14:27:26 +0530

    Create 796-RotateString

---
+++ 796-RotateString/version/java/796-RotateString_20260401_142725.java
@@ -0,0 +1,15 @@
+// Last updated: 4/1/2026, 2:27:25 PM
+1class Solution {
+2    public boolean rotateString(String s, String goal) {
+3        if (s.length() != goal.length()) return false;
+4        int n = s.length();
+5        StringBuilder sb = new StringBuilder(s);
+6        for (int i=0; i<n; i++) {
+7            char firstChar = sb.charAt(0);
+8            sb.deleteCharAt(0);
+9            sb.append(firstChar);
+10            if (sb.toString().equals(goal)) return true;
+11        }
+12        return false;
+13    }
+14}
