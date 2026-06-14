a157de00c82985d37e6bd6201b2c963f453cb32d
Sourabh Lahoti <149955947+SourabhLahoti15@users.noreply.github.com>
2026-06-14 10:03:29 +0530

    Create 4312-LimitOccurrencesInSortedArray

---
+++ 4312-LimitOccurrencesInSortedArray/4312-LimitOccurrencesInSortedArray.java
@@ -0,0 +1,18 @@
+// Last updated: 6/14/2026, 10:03:41 AM
+class Solution {
+    public int[] limitOccurrences(int[] nums, int k) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int count = 1;
+        list.add(nums[0]);
+        for (int i=1; i<nums.length; i++) {
+            if (nums[i] == nums[i-1]) count++;
+            else count = 1;
+            if (count <= k) list.add(nums[i]);
+        }
+        int[] ans = new int[list.size()];
+        for (int i=0; i<list.size(); i++) {
+            ans[i] = list.get(i);
+        }
+        return ans;
+    }
+}
