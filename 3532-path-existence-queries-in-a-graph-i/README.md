692466224384aa3cc172bf0cfb464efc7ef226c3
Sourabh Lahoti <149955947+SourabhLahoti15@users.noreply.github.com>
2026-07-10 18:50:27 +0530

    3532-path-existence-queries-in-a-graph-i: accepted in Java | Time 459 ms | Memory 162.9 MB

--- 3532-path-existence-queries-in-a-graph-i/README.md
+++ 3532-path-existence-queries-in-a-graph-i/README.md
@@ -2,5 +2,5 @@
 
 Difficulty: Medium  
 Language: Java  
-Submitted: 2026-07-09T16:59:15.000Z  
+Submitted: 2026-07-10T13:20:20.000Z  
 LeetCode: https://leetcode.com/problems/path-existence-queries-in-a-graph-i/
--- 3532-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.java
+++ 3532-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.java
@@ -1,32 +1,20 @@
 class Solution {
-    int parent[];
     public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
-        parent = new int[n];
-        for(int i=0; i<n ;i++) {
-            parent[i]=i; }                        // each node is its own root initially
-        for(int i=0; i<n-1; i++) {
-            int num1 = nums[i], num2 = nums[i+1];  // only consecutive pairs can be edges
-
-            if(Math.abs(num1 - num2) <= maxDiff) {
-                union(i, i+1);                     // within gap -> merge the two nodes
+        int[] component = new int[n];
+        for (int i=1; i<nums.length; i++) {
+            if (nums[i] - nums[i-1] <= maxDiff) {
+                component[i] = component[i-1];
+            } else {
+                component[i] = i;
             }
         }
-        boolean ans[] = new boolean[queries.length];
-        int index = 0;
-        for(int q[] : queries) {
-            int x = q[0];
-            int y = q[1];
-
-            ans[index++] = (find(x)==find(y));     // same root -> path exists
+        boolean[] answer = new boolean[queries.length];
+        for (int i=0; i<queries.length; i++) {
+            int u = queries[i][0];
+            int v = queries[i][1];
+            if (component[u] == component[v]) 
+                answer[i] = true;
         }
-        return ans;
-    }
-    private int find(int x) {
-        if(parent[x]!=x)
-            parent[x] = find(parent[x]);           // path compression: point straight to root
-        return parent[x];
-    }
-    private void union(int x, int y) {
-        parent[find(x)] = find(y);                 // hang x's root under y's root
+        return answer;
     }
 }
