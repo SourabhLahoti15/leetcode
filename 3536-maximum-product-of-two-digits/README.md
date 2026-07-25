728a20789e5ce5a696d0e3d7c48ba7f37f4aab7d
Sourabh Lahoti <149955947+SourabhLahoti15@users.noreply.github.com>
2026-07-25 22:01:21 +0530

    3536-maximum-product-of-two-digits: accepted in Java | Time 1 ms | Memory 42.5 MB

---
+++ 3536-maximum-product-of-two-digits/README.md
@@ -0,0 +1,6 @@
+# Maximum Product of Two Digits
+
+Difficulty: Easy  
+Language: Java  
+Submitted: 2026-07-25T16:31:15.000Z  
+LeetCode: https://leetcode.com/problems/maximum-product-of-two-digits/
---
+++ 3536-maximum-product-of-two-digits/maximum-product-of-two-digits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProduct(int n) {
+        int first=0, second=0;
+        while (n > 0) {
+            int ld = n % 10;
+            if (ld > first) {
+                second = first;
+                first = ld;
+            } else if (ld > second) {
+                second = ld;
+            }
+            n = n / 10;
+        }
+        return first*second;
+    }
+}
