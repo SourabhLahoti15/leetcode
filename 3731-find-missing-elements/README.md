40af32e33c711ac0adfb6046c4d6d360558030e1
Sourabh Lahoti <149955947+SourabhLahoti15@users.noreply.github.com>
2026-08-04 21:44:50 +0530

    3731-find-missing-elements: accepted in Java | Time 1 ms | Memory 46.9 MB

---
+++ 3731-find-missing-elements/README.md
@@ -0,0 +1,6 @@
+# Find Missing Elements
+
+Difficulty: Easy  
+Language: Java  
+Submitted: 2026-08-04T16:14:43.000Z  
+LeetCode: https://leetcode.com/problems/find-missing-elements/
---
+++ 3731-find-missing-elements/find-missing-elements.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        int min = nums[0];
+        int max = nums[1];
+        int n = nums.length;
+        for (int i=0; i<n; i++) {
+            min = Math.min(min, nums[i]);
+            max = Math.max(max, nums[i]);
+        }
+        boolean[] a = new boolean[max - min + 1];
+        for (int i=0; i<n; i++) {
+            a[nums[i] - min] = true;
+        }
+        List<Integer> l = new ArrayList<>();
+        for (int i=0; i<a.length; i++) {
+            if (!a[i]) {
+                l.add(i+min);
+            }
+        }
+        return l;
+    }
+}
